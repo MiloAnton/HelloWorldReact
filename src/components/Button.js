7703d18c7fe76fefa2d193014614da2db80c6fc2
MiloAnton <milo.roche@outlook.com>
2021-12-14 10:46:43 +0100

    Add button hover

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,18 +1,13 @@
 import React from "react";
+import './button.css';
 
 export default function Button({onClick:oCEX, value}) {
     return ( 
         <button 
             onClick={oCEX}
-            style={{ 
-                backgroundColor:"#B884FF",
-                marginTop:"33px", 
-                height:"50px",
-                width:"250px",
-                borderRadius:"13px",
-                fontSize:"18px"
-        }}>
+            className="buttoncss"
+        >
             {value}
-        </button> 
+        </button>
     );
 }
